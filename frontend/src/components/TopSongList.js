b1b6ad886548088845c05ee7e87231be384e595b
Tara MacKinnon <taramackinnon4@gmail.com>
2023-08-16 17:06:44 -0700

    add buttons for time ranges

--- frontend/src/components/TopSongList.js
+++ frontend/src/components/TopSongList.js
@@ -1,11 +1,17 @@
-import './TopSongList.scss';
 import { useEffect, useState } from 'react';
 import SongItem from './SongItem';
-import Table from 'react-bootstrap/Table'
+import Table from 'react-bootstrap/Table';
+import ToggleButton from 'react-bootstrap/ToggleButton';
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup';
 
 export default function TopSongList(props) {
 
-  const [topSongs, setTopSongs] = useState([])
+  const [topSongs, setTopSongs] = useState([]);
+  const [clickedButtons, setClickedButtons] = useState({
+    short_term: true,
+    medium_term: false,
+    long_term: false
+  });
   const spotifyApi = props.spotifyApi;
   const accessToken = props.accessToken
 
@@ -25,6 +31,24 @@ export default function TopSongList(props) {
       });
   }, [accessToken, spotifyApi]);
 
+  const updateTimeRange = function(time_range) {
+    const buttons = {
+      short_term: false,
+      medium_term: false,
+      long_term: false
+    };
+    buttons[time_range] = true;
+    setClickedButtons(buttons);
+  }
+
+  const buttons = Object.keys(clickedButtons).map((button, index) => {
+    return (
+        <ToggleButton className='rounded-0' key={index} id={button} value={index} onClick={() => {updateTimeRange(button)}}>
+          {button}
+        </ToggleButton>
+    )
+  })
+
   const songs = topSongs.map((song, index) => {
     return (
       <SongItem
@@ -40,15 +64,18 @@ export default function TopSongList(props) {
   })
 
   return (
-    <div className='top-song-list'>
-      <h1>My Recent Favourite Songs</h1>
-      <Table striped bordered hover variant='dark' responsive className='top-song-list-table'>
+    <div className='w-50 m-5 h-50'>
+      <h1 className='fw-bold'>Top Songs</h1>
+      <ToggleButtonGroup type="radio" name="time-range-options" defaultValue={0}>
+        {buttons}
+      </ToggleButtonGroup>
+      <Table striped bordered hover responsive className='shadow'>
         <thead>
           <tr>
-            <th>#</th>
+            <th className='text-center'>#</th>
             <th>Title</th>
             <th>Album</th>
-            <th>Length</th>
+            <th className='text-end'>Length</th>
           </tr>
         </thead>
         {songs}
