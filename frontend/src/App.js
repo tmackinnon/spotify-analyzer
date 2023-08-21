480ff20377c4e95f1faede9052df085efc3a1b48
Tara MacKinnon <taramackinnon4@gmail.com>
2023-08-20 21:01:37 -0700

    add nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Dashboard from './components/Dashboard';
 import Login from './components/Login';
 import { useState } from 'react';
+import Nav from './components/Nav';
 
 
 export default function App() {
 
-  // const [name, setName] = useState('')
-
   const params = new URLSearchParams(window.location.search)
   const code = params.get('code');
-  console.log('code', code);
   
   return (
     <div className="App">
-      <header className='bg-success text-white d-flex p-4'>
-        <h1 className='fw-bold'>Spotify Analyzer</h1>
-        {/* {name ? <h1 className='fw-bold m-3'>{name}'s Dashboard</h1> : ''} */}
-      </header>
+      <Nav/>
       {code ? <Dashboard code={code}/> : <Login/>}
     </div>
   );
---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,8 @@
+export default function Nav(props) {
+
+  return (
+    <header className='bg-success text-white d-flex p-4'>
+      <h1 className='fw-bold'>Spotify Analyzer</h1>
+    </header>
+  )
+}
